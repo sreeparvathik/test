1c6b35315a13950d3b66d4ecf9bcc555eb0548a3
Sreeparvathi K <sreeparvathik97@gmail.com>
2021-07-01 09:51:06 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,6 +1,7 @@
 <?php
 $mysqli = new mysqli("sql6.freesqldatabase.com","sql6422177","dKNFHXkf6Z","sql6422177");
-
+console.log("mysqli",$mysqli
+            )
 // Check connection
 if ($mysqli -> connect_errno) {
   echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
